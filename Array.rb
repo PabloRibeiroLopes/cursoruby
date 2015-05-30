588c4408c72d5623e50b484dcb9546db6e51f56a
Pablo <pablo@nowid.com.br>
2015-05-30 14:14:44 -0300

    Versao 1.0

---
+++ Array.rb
@@ -0,0 +1,10 @@
+#Array
+nomes = ["Ruby","Java","C","PHP"]
+puts nomes
+nomes << "Pacal"
+puts nomes
+#Hash
+pessoa = {:nome=>"Pablo", :email=>"Pablo@nowid.com.br"}
+puts pessoa [:nome]
+puts pessoa [:email]
+puts pessoa
---
+++ numeros.rb
@@ -0,0 +1,9 @@
+idade = 10
+salario = 1200.90
+populacao = 1_000_000_000
+
+soma = 1+1
+sub = 3-1
+multi = 2*3
+div = 4/2
+resto = 10%3
